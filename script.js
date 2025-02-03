aab0ad5ceb4e98fa1c7f6ebbbeb4b5f67c071e63
Satyobroto <satyo97du@gmail.com>
2025-02-03 13:58:08 +0100

    Updated object Opgave 2

--- script.js
+++ script.js
@@ -1,11 +1,38 @@
+//----------Opgave 1 ----------------------
+// const myObject = {
+//   name: "Object",
+//   id: 101,
+//   isColor: false,
+//   place: "Copenhagen"
+// }
+
+// console.log(myObject.name)
+// console.log(myObject.id)
+// console.log(myObject["isColor"])
+// console.log(myObject["place"])
+
+
+
+//------------ Opgave 2------------
+
 const myObject = {
   name: "Object",
   id: 101,
   isColor: false,
-  place: "Copenhagen"
+  objectitems: ["Read", "Write", "Exam", "Pass"],
+  place: {
+    street: "Vestre Kvartregade",
+    city:"Copenhagen",
+    country: "Denmark"
+  }
 }
 
-console.log(myObject.name)
-console.log(myObject.id)
-console.log(myObject["isColor"])
-console.log(myObject["place"])
+//-- Output for array ----------
+
+myObject.objectitems.forEach(item => console.log(item))
+
+//------- Output for object -----------
+
+console.log(myObject.place.city);
+console.log(myObject.place["country"]);
+
